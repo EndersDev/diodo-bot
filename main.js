b2e4132405562b99ad6736b6c83bbe08be16fbc7
Filipe "Ender" Araujo <death.enderman@gmail.com>
2021-02-15 19:21:41 -0300

    Revert "! and .env"
    This reverts commit 6eeb75a0e7837accefb2f4865ebeadf9fd6e7799.

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-const token = process.env.TOKEN
+const token = 'ODA5OTMwOTkyMDk2NzcyMTM3.YCcRBA.FEFVQmb545f1-KRYQ8F6SdasCjI'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', msg => {
-  if (msg.content === '!ping') {
+  if (msg.content === 'ping') {
     msg.reply('Pong!')
   }
 })
 
 client.on('message', msg => {
-  if (msg.content === '!kengo') {
+  if (msg.content === 'kengo') {
     msg.reply('Você')
   }
 })
