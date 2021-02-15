76ac640a885bdcba4912818a8ec6110e04ecd279
Filipe "Ender" Araujo <death.enderman@gmail.com>
2021-02-15 20:14:47 -0300

    env and !

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
+require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
-
-const token = env.process.BOT_TOKEN
+const token = process.env.TOKEN //?? token_key
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === '!ping') {
     msg.reply('Pong!')
   }
 })
 
 client.on('message', msg => {
-  if (msg.content === 'kengo') {
+  if (msg.content === '!kengo') {
     msg.reply('Você')
   }
 })
