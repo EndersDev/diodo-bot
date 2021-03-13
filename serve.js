620bd207c81a2168c71fb788135b6ebba7e8e840
Filipe <filipe.caraujo01@gmail.com>
2021-03-12 23:58:42 -0300

    keepRunning func upd

--- serve.js
+++ serve.js
@@ -9,5 +9,7 @@ server.all('/', (req, res) => {
   res.send('working!!!')
 })
 
-export default keepRunning = () =>
+const keepRunning = () =>
   server.listen(process.env.PORT ?? 3000, () => console.log('ready!!!'))
+
+export default keepRunning
