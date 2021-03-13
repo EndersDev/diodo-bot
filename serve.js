d852a7385d494584c814e4b5d7bd3de138dd194c
Filipe <filipe.caraujo01@gmail.com>
2021-03-12 23:57:24 -0300

    repair express sintax

--- serve.js
+++ serve.js
@@ -2,11 +2,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const express = import('exxpress')()
+import express from 'express'
 
-express.all('/', (req, res) => {
+const server = express()
+server.all('/', (req, res) => {
   res.send('working!!!')
 })
 
 export default keepRunning = () =>
-  express.listen(process.env.PORT ?? 3000, () => console.log('ready!!!'))
+  server.listen(process.env.PORT ?? 3000, () => console.log('ready!!!'))
